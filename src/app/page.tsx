475e91eb5efc471583826f9cdca0ef421bf71d59
Matty Serwer <Matthew.Serwer@gmail.com>
2025-10-30 02:46:44 -0400

    Installs shadcn 3.5. Adds Components and Lib directories. Adds prettier to eslint.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="text-3xl font-bold">
       <span>Hello There. This Is LTDe</span>
+      <Button>Shads</Button>
     </div>
-  )
+  );
 }
-

30b9aa5e51b5d64052acfea35b8056095365dd52
Matty Serwer <Matthew.Serwer@gmail.com>
2025-10-30 01:36:13 -0400

    Installs Next & Tailwind. Creates SRC directory.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold">
+      <span>Hello There. This Is LTDe</span>
+    </div>
+  )
+}
+
